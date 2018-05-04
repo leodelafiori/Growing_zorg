3f89503719ed4ecc78a4c7a8acea7d7820167da7
leodelafiori <leodelafiori@users.noreply.github.com>
2018-05-04 20:32:44 -0300

    Criacao da base

---
+++ Assets/Scripts/Spawna_inimigos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawna_inimigos : MonoBehaviour {
+
+    // Criando array de game object
+    public GameObject[] prefabs;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
