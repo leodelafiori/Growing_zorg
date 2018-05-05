32bbe4aa82827a2801bf0dfe7f8e485f2fb5e6ba
leopaivaleo <38993618+leopaivaleo@users.noreply.github.com>
2018-05-05 01:56:18 -0300

    assgarg

--- Assets/Scripts/Prefabs_cientista.cs
+++ Assets/Scripts/Prefabs_cientista.cs
@@ -2,27 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Prefabs_cientista : MonoBehaviour {
+public class Prefabs_cientista : MonoBehaviour
+{
     public float velocidade;
     public bool direcao;
     public Rigidbody rb;
     private Animator animator;
-
+    public GameObject Player;
+    public GameObject cientista;
+    
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = gameObject.transform.GetComponent<Animator>();
+        Player = GameObject.Find("Player");
     }
-           
+
     // Update is called once per frame
     void Update()
     {
         float translation = Time.deltaTime * 10;
         transform.Translate(-velocidade * Time.deltaTime, 0, 0);
-
-
     }
 
-    
+    /*
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.tag == "Player_vermelho")
+        {
+            Destroy(cientista);
+        }
+    }
 }
