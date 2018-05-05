fea4423c6200cc450f7918d6234040501fc789db
leopaivaleo <38993618+leopaivaleo@users.noreply.github.com>
2018-05-04 22:15:52 -0300

    script cientista

--- Assets/Scripts/Prefabs_cientista.cs
+++ Assets/Scripts/Prefabs_cientista.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Prefabs_cientista : MonoBehaviour {
+    public float velocidade;
+    public bool direcao;
+    public Rigidbody rb;
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = gameObject.transform.GetComponent<Animator>();
+    }
+           
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Time.deltaTime * 10;
+        transform.Translate(-velocidade * Time.deltaTime, 0, 0);
+
+
+    }
+
+    
 }
